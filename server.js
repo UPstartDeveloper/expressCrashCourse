aaddb31553f3ceb6d90e8b9fefa534cac8716c23
= <zainr7989@gmail.com>
2020-01-01 12:30:03 -0500

    [Add] Route handler for root directory.

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+// route for the root directory
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Project!</h1>');
+})
+
+// give it a PORT to listen to
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

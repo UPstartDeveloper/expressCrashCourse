df85c356a11281a6c914c5fbfdf16636e6750ce8
= <zainr7989@gmail.com>
2020-01-02 06:38:06 -0500

    [Add] Handlebars home page view.

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const logger = require('./middleware/logger');
 // Basic Server Syntax in Express
 const express = require('express');
 const app = express();
+// import Handlebars
+const exphbs = require('express-handlebars');
 
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+// Route for index page
+app.get('/', (req, res) => res.render('index'));
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+/*
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+*/
 
 // Integrate API: Members routes
 app.use('/api/members', require('./routes/api/members'));
